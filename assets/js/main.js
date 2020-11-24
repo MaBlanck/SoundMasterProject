cfc754f03e8d13cf277da4806ed9bd91d20647cc
Ping YANG <ping.yang.dev@gmail.com>
2020-11-24 16:36:01 +0100

    Ajout données catalogue

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,22 @@
 let catalog = new Catalog();
 
 let data = [
-    ['G001', 'Guitare Fender', 'Guitare master', 'assets/img/imgf.jpg', 4500],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G003', 'Guitare Squier', 'Premier prix', 'assets/img/imgc.jpg', 500]
+    ['G001', 'Ovation Celebrity', 'CE48P-TGE Elite Plus', 'assets/img/guit.jpg', 498],
+    ['G002', 'Harley Benton Custom Line', 'CLJ-412E NT', 'assets/img/guit.jpg', 248],
+    ['G003', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['G004', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['K002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['K002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['K002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['K002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['B002', 'Thomann Trumpet', 'ETR-3300S Eb/D', 'assets/img/trompette.png', 999],
+    ['B002', 'Arnolds & Sons Tuba', 'Tuba 25', 'assets/img/Tuba.png', 39],
+    ['B002', 'Startone Sax', 'SAS-75 Alto Sax', 'assets/img/saxophone alto.png', 244],
+    ['B002', 'Startone Sax', 'STS-75 Tenor Sax', 'assets/img/Saxophone tenor.png', 2000],
+    ['D002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['D002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['D002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['D003', 'Guitare Squier', 'Premier prix', 'assets/img/imgc.jpg', 500]
 ];
 
 data.forEach(element => {
