aaa8e09ff3d3af5766da2ebb4a8441d03de0a0ea
Ping YANG <ping.yang.dev@gmail.com>
2020-11-25 11:46:05 +0100

    Merge - footer

--- assets/js/Cart.js
+++ assets/js/Cart.js
@@ -1,6 +1,79 @@
+/*
+    Classe Cart
+    représente le panier
+*/
 class Cart extends Array {
+    catalog;
+
+    // Constructeur, prend en paramètre le catalogue peuplé au préalable
+    constructor(catalog) {
+        this.catalog = catalog;
+    }
+
+    // Appelé lors d'un ajout au panier
     addProduct(reference, quantity) {
-        this.push([reference, quantity]);
-        let orderList = document.getElementById('orderList');
+        this.push({"reference": reference, "quantity": quantity});
+    }
+
+    addItem(itemIndex) {
+        this[itemIndex].quantity++;
+        this.updateOrderList();
+    }
+    removeItem(itemIndex) {
+        this[itemIndex].quantity--;
+        this.updateOrderList();
+    }
+
+    // Mise à jour dynamique de la liste de commande
+    updateOrderList() {
+        // Récupération du <tbody> contenant la liste des achats
+        let orderList = document.getElementById('order-list');
+        orderList.innerHTML = '';
+
+        // Remplissage de la liste de commande
+        for (let currentProduct = 0; currentProduct < this.length; currentProduct++) {
+            // Récupération des infos pour le produit en cours
+            let orderItem = catalog.getProduct(this[currentProduct].reference);
+
+            // Création des éléments
+            let orderLine = document.createElement('tr');
+            let description = document.createElement('td');
+            let price = document.createElement('td');
+            let quantity = document.createElement('td');
+            let addItemBtn = document.createElement('button');
+            let removeItemBtn = document.createElement('button');
+            let subTotal = document.createElement('td');
+            let removeBtnCol = document.createElement('td');
+            let removeBtn = document.createElement('button');
+
+            // Remplissage des éléments
+            description.textContent = orderItem.description;
+            price.textContent = orderItem.price;
+            subTotal.textContent = this[currentProduct].quantity * orderItem.price;
+            // Création des boutons pour augmenter/diminuer la quantité
+            let quantityText = document.createTextNode(this[currentProduct].quantity);
+            removeItemBtn.addEventListener('click', () => this.removeItem(currentProduct));
+            removeBtn.textContent = '-';
+            addItemBtn.addEventListener('click', () => this.addItem(currentProduct));
+            addItemBtn.textContent = '+';
+            quantity.appendChild(addItemBtn);
+            quantity.appendChild(quantityText);
+            quantity.appendChild(removeItemBtn);
+            
+            // Création du bouton pour retirer le produit
+            removeBtn.addEventListener('click', () => this.removeProduct(currentProduct));
+            removeBtn.textContent = '&times;';
+            removeBtnCol.appendChild(removeBtn);
+
+            // Ajout des éléments à la ligne
+            orderLine.appendChild(description);
+            orderLine.appendChild(price);
+            orderLine.appendChild(quantity);
+            orderLine.appendChild(subTotal);
+            orderLine.appendChild(removeBtnCol);
+
+            // Ajout de la ligne à la liste de commande
+            orderList.appendChild(orderLine);
+        }
     }
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,17 @@ let catalog = new Catalog();
 
 let data = [
     ['G001', 'Ovation Celebrity', 'CE48P-TGE Elite Plus', 'assets/img/guit.jpg', 498],
-    ['G002', 'Harley Benton Custom Line', 'CLJ-412E NT', 'assets/img/guit.jpg', 248],
-    ['G003', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['G004', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['K001', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['K002', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['K003', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
-    ['K004', 'Guitare Cort', 'Guitare OK', 'assets/img/imgg.jpg', 2000],
+    ['G002', 'Harley Benton Custom Line', 'CLJ-412E NT', 'assets/img/guitar.jpg', 248],
+    ['G003', 'Gibson SG', 'Standard ´64 Maestro CH VOS', 'assets/img/guitElec.jpg', 545],
+    ['G004', 'Gibson SG', 'Standard ´64 Maestro CH VOS', 'assets/img/Guitelec2.jpg', 1379],
+    ['K001', 'KAWAI GL 10', 'E/P Grand Piano', 'assets/img/piano.jpg', 9799],
+    ['K002', 'Thomann DP-95', 'B Arranger Set', 'assets/img/piano2.jpg', 599],
+    ['K003', 'Novation 61SL', 'MKIII', 'assets/img/clavier.jpg', 599],
+    ['K004', 'Swissonic', 'EasyKey 61', 'assets/img/clavier2.jpg', 99],
     ['B001', 'Thomann Trumpet', 'ETR-3300S Eb/D', 'assets/img/trompette.png', 999],
     ['B002', 'Arnolds & Sons Tuba', 'Tuba 25', 'assets/img/Tuba.png', 39],
     ['B003', 'Startone Sax', 'SAS-75 Alto Sax', 'assets/img/saxophone alto.png', 244],
-    ['B004', 'Startone Sax', 'STS-75 Tenor Sax', 'assets/img/Saxophone tenor.png', 2000],
+    ['B004', 'Startone Sax', 'STS-75 Tenor Sax', 'assets/img/Saxophone tenor.png', 398],
     ['D001', 'Mapex Tornado Stand.', 'Zildjian Set BK', 'assets/img/Batterie .png', 499],
     ['D002', 'Clifton Bongo Set', 'Bongos', 'assets/img/bongo.png', 19.60],
     ['D003', 'Sonor Xylophone', 'SX CB F Soprano Xylophone', 'assets/img/Xylophone soprano.png', 369],
@@ -27,4 +27,4 @@ data.forEach(element => {
 });
 
 // Initialisation du panier
-let cart = new Array();
+let cart = new Cart(catalog);
